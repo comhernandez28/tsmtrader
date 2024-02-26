af2da00489a0cd34b716c15be907cbf09e2d39bc
Omar Hernandez <comhernandez28@gmail.com>
2024-02-26 10:33:42 -0800

    added readme and gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('intial');
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const port = 3000;
+
+const app = express();
+
+app.listen(port, () => {
+	console.log(`Server started on port ${port}`);
+});
